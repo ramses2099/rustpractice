4791f11e3bcb33ad7a4ba582c84e0e9d2bafb2b5
ramses2099 <ramses2099@gmail.com>
2025-03-24 09:41:27 -0400

    latest changes in md file

--- README.md
+++ README.md
@@ -14,5 +14,15 @@ cargo-modules structure --lib
 # create new branch
 git checkout -b feature
 git push origin 
+git push --set-upstream origin feature
 
 # create pull request
+
+# Container Result
+use std::{num::ParseIntError, result::Result};
+use std::option::Option;
+
+- unwrap extracts a value in the container if present.
+- .ok converts between Result and Option.
+- .map converts to the same container but with a different type.
+- ? operator is used to propagate the absent of a value or a success up the stack.
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,30 @@
+
+use std::{num::ParseIntError, result::Result};
+// use std::option::Option;
+
+
+
+fn to_int(s: &str) -> Result<i32, ParseIntError> {
+    s.parse()
+}
+
+
+fn sum_str_vec(strs:Vec<String>) -> Result<String, ParseIntError>{
+    let mut accum = 0;
+    for s in strs {
+        accum += to_int(&s)?; //nError)?; //
+    }
+    Ok(accum.to_string())
+}
+
+
 fn main() {
-    println!("Hello, world!");
+    let v = vec![String::from("3"),String::from("4")];
+    let total = sum_str_vec(v);
+    println!("{:?}", total);
 
-
-    
+    let v = vec![String::from("3"),String::from("abc")];
+    let total = sum_str_vec(v);
+    println!("{:?}", total);
+   
 }
