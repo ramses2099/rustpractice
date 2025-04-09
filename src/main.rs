96c61e315a663a04403066d59d5267757ab5f246
ramses2099 <ramses2099@gmail.com>
2025-04-09 19:03:32 -0400

    latest changes

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,56 @@
 #![allow(dead_code)]
 
-#[derive(Debug)]
-struct Person {
-    name: String,
-    real_name: String,
-    heigth: u8,
-    happy: bool,
-}
-fn main() -> Result<(), String> {
-    let papa_doc = Person {
-        name: "Papa Doc".to_string(),
-        real_name: "Clarences".to_string(),
-        heigth: 170,
-        happy: false,
-    };
+// GENERICS (in functions)
+// generic = can be one type, can be another type
+// concrete = is one type
+// angle brackets <T>
+// T, U, V - one capital letter
 
-    println!("{:?}", papa_doc);
-    // Destructure struct
-    let Person { name, real_name, heigth, happy } = papa_doc;
-    println!("name {}, real name {}", name, real_name);
+use std::fmt::{ Debug, Display };
+use std::cmp::{ PartialOrd };
+
+// fn return_number(number: i32) -> i32{
+//     println!("Here is the return number {}", number);
+//     number
+// }
+fn return_number<T>(number: T) -> T {
+    println!("Here is the thing");
+    number
+}
+
+fn print_number<T: Display>(number: T) {
+    println!("Here is your number: {}", number);
+}
+
+#[derive(Debug)]
+struct Animal {
+    name: String,
+    age: u8,
+}
+
+fn print_item<T: Debug>(item: T) {
+    println!("Here is your item {:?}", item)
+}
+
+fn compare_and_display<T: Display, U: Display + PartialOrd>(statement: T, num_1: U, num_2: U) {
+    println!("{}! is {} greated than {}? {}", statement, num_1, num_2, num_1 > num_2);
+}
+
+fn main() -> Result<(), String> {
+    let n = return_number("String".to_string());
+    println!("{}", n);
+    //
+    print_number(32);
+
+    let charlie = Animal {
+        name: String::from("Charlie"),
+        age: 1,
+    };
+    //
+    print_item(charlie);
+
+    //
+    compare_and_display("test for compare".to_string(), 10, 12);
 
     Ok(())
 }
